eea4c77c3cf70fd40587902ca2a71d3a019fde37
cgajean@student.42.fr <cgajean@f6r9s11.clusters.42paris.fr>
2025-09-01 15:36:23 +0200

    Parsing en cours (non fonctionnel)

--- srcs/parsing/build_ast.c
+++ srcs/parsing/build_ast.c
@@ -6,7 +6,7 @@
 /*   By: cgajean <cgajean@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 14:10:20 by ihadj             #+#    #+#             */
-/*   Updated: 2025/09/01 11:38:51 by cgajean          ###   ########.fr       */
+/*   Updated: 2025/09/01 15:08:34 by cgajean          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 // j ai lexee (tokeniser), ensuite j ai check les erreurs de syntax, 
 // mtn je dois parser pour definir si les element sont des REDIR-IN etc ...
 
-static void	create_node(t_ast_p ast, t_token_p token)
+/* static void	create_node(t_ast_p ast, t_token_p token)
 {
 	if (token->type == T_WORD)
 		;		
@@ -39,21 +39,103 @@ static void	create_node(t_ast_p ast, t_token_p token)
 		;
 	else if (token->type == T_EXPAND)	
 		;
+} */
+
+void	reach_target_token(t_arg_p **tokens, int *index)
+{
+	int			n = 0;
+	
+	while (**tokens && n < *index)
+		(*tokens)++;
 }
 
-static void	parse_tokens(t_ast_p ast, t_arg_p tok_container)
+t_sub_ast_p	find_external_cntl_and_or(t_arg_p *tokens, int *index)
 {
-	t_token_p	*tokens;
 	t_token_p	cur_token;
-
-	tokens = tok_container->tokens;
-	while (*tokens)
+	t_sub_ast_p	new_op;
+		
+	*index = 0;	
+	new_op = ft_calloc(1, sizeof(t_sub_ast));
+	if (new_op)
 	{
-		cur_token = *tokens;
-		if (cur_token->val)								// null terminated
-			printf("%s\n", cur_token->val);
-		tokens++;
+		reach_target_token(&tokens, index);
+		while (*tokens)
+		{
+			cur_token = *tokens;
+			if (ft_strcmp(cur_token->val, "&&"))
+			{
+				new_op->type == OP_AND;
+				break ;
+			}
+			else if (ft_strcmp(cur_token->val, "||"))
+			{
+				new_op->type == OP_OR;
+				break ;
+			}
+			tokens++;
+			(*index++);
+		}
+		free(new_op);
+		new_op = NULL;
 	}
+	return (new_op);
+}
+
+t_sub_ast_p	find_external_cntl_pipe(t_arg_p *tokens, int *index)
+{
+	t_token_p	cur_token;
+	t_sub_ast_p	new_op;
+	int			n = 0;
+	
+	*index = 0;	
+	new_op = ft_calloc(1, sizeof(t_sub_ast));
+	if (new_op)
+	{
+		reach_target_token(&tokens, index);
+			
+		while (*tokens)
+		{
+			cur_token = *tokens;
+			if (ft_strcmp(cur_token->val, "|"))
+			{
+				new_op->type == OP_PIPE;
+				break ;
+			}
+			tokens++;
+			(*index++);
+		}
+		free(new_op);
+		new_op = NULL;
+	}
+	return (new_op);
+}
+
+static void	parse_tokens(t_ast_p ast, t_arg_p tok_container, int *index_value)
+{
+	bool		parenthesis = 0;
+	int			index = 0, *index_ptr;
+	t_sub_ast_p	current_op;
+
+	if (index_value)
+		index_ptr = index_value;
+	else
+		index_ptr = &index;
+		
+	current_op = find_external_cntl_and_or(tok_container, index_ptr);
+	if (!current_op)
+	{		*index_ptr = 0;
+		current_op = find_external_cntl_pipe(tok_container, index_ptr);
+		if (!current_op)
+		{
+			current_op = ft_calloc(1, sizeof(t_sub_ast));
+			if (current_op)
+			{
+				;					// it's a leaf
+				return ;
+			}
+		}
+	}
+	parse_tokens(current_op, tok_container, index_ptr);
 }
 
 t_error_status	build_ast(t_ast_p ast, t_arg_p tok_container)
@@ -61,7 +143,7 @@ t_error_status	build_ast(t_ast_p ast, t_arg_p tok_container)
 	ast = ft_calloc(1, sizeof(t_ast));
 	if (ast)
 	{
-		parse_tokens(ast, tok_container);
+		parse_tokens(ast, tok_container, NULL);
 	}
 	else
 	{
