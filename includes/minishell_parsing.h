f71aa6ba858fa1706b77a309fb9d9126159a9df1
Ilies Hadj <ihadj@f6r8s11.clusters.42paris.fr>
2025-09-03 17:35:49 +0200

    pull

--- includes/minishell_parsing.h
+++ includes/minishell_parsing.h
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell_parsing.h                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgajean <cgajean@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ihadj <ihadj@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 12:48:48 by ihadj             #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/09/03 15:07:46 by ihadj            ###   ########.fr       */
-=======
-/*   Updated: 2025/09/03 15:21:32 by cgajean          ###   ########.fr       */
->>>>>>> ef9d9dae428d70487b58b75ef98fc608600b6700
+/*   Updated: 2025/09/03 17:35:13 by ihadj            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,11 +68,8 @@ t_error_status	find_external_parenthesis(t_ast_p *op, t_tok_container_p tok_cont
 t_error_status	find_external_cntl_pipe(t_ast_p *op, t_tok_container_p tok_container, int start, int end);
 t_error_status	find_external_cntl_and_or(t_ast_p *op, t_tok_container_p tok_container, int start, int end);
 
-<<<<<<< HEAD
-=======
 // t_error_status	build_cmd(char ***cmd, t_tok_container_p tok_container, int i);
 // t_redir_p 		build_redir(t_tok_container_p tok_container, int i);
->>>>>>> ef9d9dae428d70487b58b75ef98fc608600b6700
 t_error_status	create_leaf(t_ast_p *ast, t_tok_container_p tok_container, int i);
 
 
--- srcs/parsing/find_external_functions.c
+++ srcs/parsing/find_external_functions.c
@@ -6,11 +6,7 @@
 /*   By: ihadj <ihadj@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 17:22:45 by cgajean           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/09/03 16:44:30 by ihadj            ###   ########.fr       */
-=======
-/*   Updated: 2025/09/03 15:56:41 by cgajean          ###   ########.fr       */
->>>>>>> ef9d9dae428d70487b58b75ef98fc608600b6700
+/*   Updated: 2025/09/03 17:35:38 by ihadj            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,20 +27,12 @@ t_error_status	find_external_cntl_and_or(t_ast_p *op, t_tok_container_p tok_cont
 			if (cur_token->type == T_AND && !in_parenthesis)
 			{
 				(*op)->type = OP_AND;
-<<<<<<< HEAD
-				return (tok_container->op_index = start, RETURN_OK) ;
-=======
 				return (free(tok_container->tokens[start]), tok_container->tokens[start] = NULL, tok_container->op_index = start, RETURN_OK) ;
->>>>>>> ef9d9dae428d70487b58b75ef98fc608600b6700
 			}
 			else if (cur_token->type == T_OR && !in_parenthesis)
 			{
 				(*op)->type = OP_OR;
-<<<<<<< HEAD
-				return (tok_container->op_index = start, RETURN_OK) ;
-=======
 				return (free(tok_container->tokens[start]), tok_container->tokens[start] = NULL, tok_container->op_index = start, RETURN_OK) ;
->>>>>>> ef9d9dae428d70487b58b75ef98fc608600b6700
 			}
 			else if (cur_token->type == T_LPARENT && !in_parenthesis)
 				in_parenthesis = 1;
@@ -73,11 +61,7 @@ t_error_status	find_external_cntl_pipe(t_ast_p *op, t_tok_container_p tok_contai
 			if (cur_token->type == T_PIPE && !in_parenthesis)
 			{
 				(*op)->type = OP_PIPE;
-<<<<<<< HEAD
-				return (tok_container->op_index = start, RETURN_OK) ;
-=======
 				return (free(tok_container->tokens[start]), tok_container->tokens[start] = NULL, tok_container->op_index = start, RETURN_OK) ;
->>>>>>> ef9d9dae428d70487b58b75ef98fc608600b6700
 			}
 			else if (cur_token->type == T_LPARENT && !in_parenthesis)
 				in_parenthesis = 1;
@@ -103,11 +87,7 @@ t_error_status	find_external_parenthesis(t_ast_p *op, t_tok_container_p tok_cont
 		{
 			tok_container->op_index++;
 			(*op)->type = OP_SUBSHELL;
-<<<<<<< HEAD
-			return (RETURN_OK) ;
-=======
 			return (free(*tok_container->tokens), *tok_container->tokens = NULL, RETURN_OK) ;
->>>>>>> ef9d9dae428d70487b58b75ef98fc608600b6700
 		}
 	}
 	return (RETURN_FAIL);	
