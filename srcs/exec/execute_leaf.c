c8a9e4b42409633a46ce30b611950263da3fdf73
cgajean@student.42.fr <cgajean@f6r10s6.clusters.42paris.fr>
2025-09-06 19:00:48 +0200

    ajout de quelques commentaires

--- srcs/exec/execute_leaf.c
+++ srcs/exec/execute_leaf.c
@@ -6,7 +6,7 @@
 /*   By: cgajean <cgajean@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/06 15:07:24 by cgajean           #+#    #+#             */
-/*   Updated: 2025/09/06 18:43:32 by cgajean          ###   ########.fr       */
+/*   Updated: 2025/09/06 19:00:31 by cgajean          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,14 @@ void	execute_builtin(void)
 	return ;
 }
 
+// la table de pid, est allouee pour PID_TABLE_SIZE lignes.
 //	La table de pid doit etre resize (+= PID_TABLE_SIZE) lorsqu'elle est pleine 
-static void	execute_fork(t_minishell_p shell,t_ast_p ast)
+
+// aucune prise en compte des pipe et des subshell a ce stade
+
+// en revanche, la priorite fonctionne pour des commandes type celles qui sont dans le fichier parsing_ok.txt
+
+static void	execute_fork(t_minishell_p shell, t_ast_p ast)
 {
 	pid_t	*pid;
 
@@ -38,6 +44,7 @@ static void	execute_fork(t_minishell_p shell,t_ast_p ast)
 		shell->exec_var.cur_index++;
 }
 
+// tres basique, permet juste de visualiser l'execution de commandes simples
 int	execute_leaf(t_minishell_p shell, t_ast_p ast)
 {
 	if (is_builtin())
