1b81c104ca130dc5eb8a78b730e8e9d8edbe1424
Cricriiii <cest_bon_les_pates@hotmail.com>
2025-09-26 12:04:50 +0200

    Correction ishidden pour suppression invalid read of size 1

--- srcs/wildcards/utils/ishidden.c
+++ srcs/wildcards/utils/ishidden.c
@@ -6,7 +6,7 @@
 /*   By: fox <fox@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 13:54:08 by fox               #+#    #+#             */
-/*   Updated: 2025/09/23 20:08:14 by fox              ###   ########.fr       */
+/*   Updated: 2025/09/26 12:02:44 by fox              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 
 bool	ishidden(char *dir)
 {
-	if (!ft_strncmp(dir, "./", 2))
-		return (ishidden(dir + 2));
-	else if (!ft_strncmp(dir, "../", 3))
-		return (ishidden(dir + 3));
-	else if (dir && *dir == '.')
-		return (1);
+	if (dir && *dir)
+	{
+		if (!ft_strncmp(dir, "./", 2))
+			return (ishidden(dir + 2));
+		else if (!ft_strncmp(dir, "../", 3))
+			return (ishidden(dir + 3));
+		else if (dir && *dir == '.')
+			return (1);
+		else
+			return (0);
+	}
 	else
 		return (0);
 }
