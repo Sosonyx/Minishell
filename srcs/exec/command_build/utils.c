c6baef8b872467b42ac848c8278bd2ab3e12b9cd
Ilies Hadj <ihadj@f6r9s11.clusters.42paris.fr>
2025-10-01 12:14:29 +0200

    wip

---
+++ srcs/exec/command_build/utils.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ihadj <ihadj@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/29 18:25:30 by ihadj             #+#    #+#             */
+/*   Updated: 2025/09/30 12:55:48 by ihadj            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	count_word(const char *str, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i++] != c)
+			count++;
+	}
+	return (count);
+}
+
+static char	*malloc_word(const char *str, char c)
+{
+	int		i;
+	char	*word;
+
+	i = 0;
+	while (str[i] && str[i] != c)
+		i++;
+	word = malloc(sizeof(char) * (i + 1));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (str[i] && str[i] != c)
+	{
+		word[i] = str[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+static void	free_tab(char **tab, int index)
+{
+	while (--index >= 0)
+		free(tab[index]);
+	free(tab);
+}
+
+char	**ft_split_path(char const *s, char c)
+{
+	char	**tab;
+	int		i;
+	int		j;
+	int		start;
+
+	if (!s)
+		return (NULL);
+	tab = malloc(sizeof(char *) * (ft_strlen(s) + 2));
+	if (!tab)
+		return (NULL);
+	i = 0;
+	j = 0;
+	start = 0;
+	while (1)
+	{
+		if (s[i] == c || s[i] == '\0')
+		{
+			if (i == start)
+				tab[j++] = ft_strdup("./");
+			else
+				tab[j++] = malloc_word(&s[start], c);
+			if (!tab[j - 1])
+				return (free_tab(tab, j), NULL);
+			if (s[i] == '\0')
+				break ;
+			start = i + 1;
+		}
+		i++;
+	}
+	tab[j] = NULL;
+	return (tab);
+}
---
+++ srcs/exec/redirections/preconfig_leaf.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   preconfig_leaf.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ihadj <ihadj@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 15:43:19 by ihadj             #+#    #+#             */
+/*   Updated: 2025/09/29 13:54:53 by ihadj            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static void	get_command_path(t_minishell_p shell, t_ast_p ast)
+{
+	char	*cmd;
+
+	ast->leaf->full_path = find_cmd(shell, ast);
+}
+
+
+static void	redir_in_conf(t_leaf_p leaf)
+{
+	char 		*target;
+	t_redir_p	cur_redir;
+
+	cur_redir = leaf->redir;
+	while (cur_redir)
+	{
+		if (cur_redir->type == R_IN || cur_redir->type == R_HDOC)
+		{
+			target = cur_redir->target;
+			leaf->r_in = true;
+			leaf->r_in_path = target;
+		}
+		cur_redir = cur_redir->next;
+	}
+}
+
+static void	redir_out_conf(t_leaf_p leaf)
+{
+	char 		*target;
+	t_redir_p	cur_redir;
+	
+	cur_redir = leaf->redir;
+	while (cur_redir)
+	{
+		if (cur_redir->type == R_OUT || cur_redir->type == R_APPEND)
+		{
+			target = cur_redir->target;
+			leaf->r_out = true;
+			leaf->r_out_path = target;
+		}
+		cur_redir = cur_redir->next;
+	}
+}
+
+void	preconfig_leaf(t_minishell_p shell, t_ast_p ast)
+{
+	redir_in_conf(ast->leaf);
+	redir_out_conf(ast->leaf);
+	get_command_path(shell, ast);
+	ast->leaf->configured = true;
+}
