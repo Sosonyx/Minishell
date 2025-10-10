3068d5cb8f6cfa41c4c8dd35aef0b1a1865d6018
Sosony <iliesvive3432@gmail.com>
2025-10-10 21:32:20 +0200

    last

--- srcs/exec/execute_subshell.c
+++ srcs/exec/execute_subshell.c
@@ -6,7 +6,7 @@
 /*   By: sosony <sosony@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 14:21:39 by cgajean           #+#    #+#             */
-/*   Updated: 2025/10/10 21:28:51 by sosony           ###   ########.fr       */
+/*   Updated: 2025/10/10 21:32:11 by sosony           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	execute_subshell(t_shell_p shell, t_ast_p ast)
 		pid = _fork(shell);
 		if (pid == 0)
 		{
+			close_secure(ast->closed_fd);
 			forward_fds(ast);
 			_execute_ast(shell, ast->cntl_op->left);
 			close_fds(ast, CHILD);
@@ -30,10 +31,8 @@ void	execute_subshell(t_shell_p shell, t_ast_p ast)
 		else if (pid > 0)
 		{
 			close_fds(ast, PARENT);
-			if (ast->cntl_op->left && ast->cntl_op->left->type == OP_PIPE)
-			{
+			if (ast->cntl_op->left)
 				close_secure(ast->cntl_op->left->closed_fd);
-			}
 			waitpid(pid, &shell->exit_code, 0);
 		}
 	}
