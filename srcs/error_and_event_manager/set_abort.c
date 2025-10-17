5361d1520c8b30d09accf6bde7bf37cec8cbe9e3
Ilies Hadj <ihadj@f6r10s9.clusters.42paris.fr>
2025-10-17 14:08:57 +0200

    fix de la remontee des signaux

--- srcs/error_and_event_manager/set_abort.c
+++ srcs/error_and_event_manager/set_abort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   set_abort.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgajean <cgajean@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ihadj <ihadj@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 16:30:50 by cgajean           #+#    #+#             */
-/*   Updated: 2025/10/07 13:50:37 by cgajean          ###   ########.fr       */
+/*   Updated: 2025/10/17 14:05:47 by ihadj            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ void	set_abort(t_shell_p shell, char *errmsg)
 	if (errmsg)
 		print_generic_error(shell, errmsg);
 	shell->abort = true;
-	shell->exit_code = ERRVAL1;
+	if (shell->exit_code != 130 && shell->exit_code != 131)
+		shell->exit_code = ERRVAL1;
 }
